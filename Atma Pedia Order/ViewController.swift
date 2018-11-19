6d364646dffeae368af887d9cebc12e6e2f9c81a
kadalcj <43379893+kadalcj@users.noreply.github.com>
2018-11-19 17:30:04 +0700

    Initial commit

---
+++ Atma Pedia Order/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Atma Pedia Order
+//
+//  Created by Sul on 2018/11/19.
+//  Copyright © 2018 Sul Compagnie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
